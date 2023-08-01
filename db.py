6ea054e5f726395937b55866b055a11634505bb7
Nitin Kayyath <55591127+NitinKnight@users.noreply.github.com>
2023-08-01 19:01:06 +0100

    Create db.py
    database info

---
+++ db.py
@@ -0,0 +1,16 @@
+# Database configurations for the application
+
+# The host IP or domain name where the database server is running
+HOST = '[YOUR_HOST]'
+
+# The username used to authenticate and access the database
+USERNAME = '[YOUR_USERNAME]'
+
+# The password associated with the given username for database access
+PASSWORD = '[YOUR_PASSWORD]'
+
+# The specific database to connect to on the server
+DATABASE = '[YOUR_DATABASE_NAME]'
+
+
+##Note: Remember to replace placeholders like `[USERNAME]`, `[PASSWORD]`, etc., with your actual details before executing.
